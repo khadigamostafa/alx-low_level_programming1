7c04ec8b1e8fef51a7213b317ffeba2fb8d3d90c
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-10 16:34:27 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function describes the main common in all c program
+ *
+ *Return: it is  always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
