98b68ed4d8331a2b99fa0e96919dec0a1a08da68
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-10 15:35:35 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-int main (void)
 #include <stdio.h>
+/**
+ * main - it descripe the main function common in all c program
+ *
+ * Return :it is  always 0
+ */
+int main(void)
 {
-
-puts("\"Programming is like building a multilingual puzzle);
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
