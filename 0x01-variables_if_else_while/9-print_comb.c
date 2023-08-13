5b05efddd3b0a40311b211526c65a273ca383b65
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-13 16:54:14 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - the main function describes the main common in all c program
+ *
+ *Return: it is  always 0
+ */
+int main(void)
+{
+	int char_c = 0;
+
+	while (char_c <= 0)
+	{
+		putchar(char_c + 48);
+		if (char_c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		char_c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
