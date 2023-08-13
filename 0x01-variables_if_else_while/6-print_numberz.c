9ee7c515842d13ce27dc1073ac04d00a4d4623e6
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-13 15:08:25 -0700

    task4

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the main function describes the main common in all c program
+ *
+ *Return: it is  always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + ‘0’);
+		num++;
+	}
+	putchar("\n");
+	return (0);
+}
