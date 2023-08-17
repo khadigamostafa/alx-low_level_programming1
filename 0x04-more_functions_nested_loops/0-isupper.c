226d8e83cb0f54de2ccfbd05c86fdb2bce4294d2
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-17 16:51:38 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check if c is upper or not
+ * 
+ *Return: it is 1 if upper
+ */
+int _isupper(int c)
+{
+	if (c >= 56 && c<= 90)
+		return (1);
+	else
+		return (0);
+
+}
