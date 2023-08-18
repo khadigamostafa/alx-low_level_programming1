2b97d9f2908141328068668388631f182066406e
khadigamostafa <eng_khadiga.2010@yahoo.com>
2023-08-18 06:03:55 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - the main function
  * _putchar - write the character c to stdout
- *@c: the character to print
+ *
  *Return: on success 0
  */
 int main(void)
@@ -10,8 +10,8 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch=0; ch<8; ch++)
+	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
-	return(0);
+	return (0);
 }
